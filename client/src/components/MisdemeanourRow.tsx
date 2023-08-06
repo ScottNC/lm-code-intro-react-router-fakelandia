3436e18babeaac20ebcb0a1c4e6543b7cbf6cdce
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-06 22:49:09 +0100

    add max width to table colums

--- client/src/components/MisdemeanourRow.tsx
+++ client/src/components/MisdemeanourRow.tsx
@@ -13,7 +13,7 @@ export const MisdemeanourRow : React.FC<Misdemeanour> = ({citizenId, date, misde
     <td className="table--row--item table--row--item--text">{citizenId}</td>
     <td className="table--row--item table--row--item--text">{date}</td>
     <td className="table--row--item table--row--item--text">{misdemeanourDisplay[misdemeanour]}</td>
-    <td className="table--row--item table--row--item--text">
+    <td className="table--row--item">
       <img src={`https://picsum.photos/200/150?random=${citizenId}`}/>
     </td>
   </tr>
