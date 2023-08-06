384ecabae4b646f99686a1608e6a090a22b34ff1
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-06 20:48:12 +0100

    adds 3 rows to the misdemeanours table

---
+++ client/src/components/MisdemeanourRow.tsx
@@ -0,0 +1,12 @@
+import { Misdemeanour } from "../types/misdemeanours.types"
+
+export const MisdemeanourRow : React.FC<Misdemeanour> = ({citizenId, date, misdemeanour}) => (<>
+  <tr className="table--row">
+    <td className="table--row--item table--row--item--text">{citizenId}</td>
+    <td className="table--row--item table--row--item--text">{date}</td>
+    <td className="table--row--item table--row--item--text">{misdemeanour}</td>
+    <td className="table--row--item table--row--item--text">
+      <img src={`https://picsum.photos/200/150?random=${Math.random()}`}/>
+    </td>
+  </tr>
+</>);
--- client/src/components/MisdemeanourTable.tsx
+++ client/src/components/MisdemeanourTable.tsx
@@ -1,4 +1,16 @@
+import { useEffect, useState } from "react";
+import { Misdemeanour } from "../types/misdemeanours.types";
+import { MisdemeanourRow } from "./MisdemeanourRow";
+
 export const MisdemeanourTable : React.FC = () => {
+
+  const [misdemeanours, setMisdemeanours] = useState<Misdemeanour[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/misdemeanours/3")
+      .then(response => response.json())
+      .then(data => setMisdemeanours(data.misdemeanours))
+  },[])
 
   return (<>
     <div className="text">
@@ -6,21 +18,12 @@ export const MisdemeanourTable : React.FC = () => {
         <tr className="table--row">
           <th className="table--row--item table--row--item--title">Citizen ID</th>
           <th className="table--row--item table--row--item--title">Date</th>
-          <th className="table--row--item table--row--item--title">Mideameanour</th>
+          <th className="table--row--item table--row--item--title">Misdeameanour</th>
           <th className="table--row--item table--row--item--title">Punishment Idea</th>
         </tr>
-        {/* <tr>
-          <td className="table--row--item table--row--item--text">Emil</td>
-          <td className="table--row--item table--row--item--text">Tobias</td>
-          <td className="table--row--item table--row--item--text">Linus</td>
-          <td className="table--row--item table--row--item--text">a</td>
-        </tr>
-        <tr>
-          <td className="table--row--item table--row--item--text">16</td>
-          <td className="table--row--item table--row--item--text">14</td>
-          <td className="table--row--item table--row--item--text">10</td>
-          <td className="table--row--item table--row--item--text">a</td>
-        </tr> */}
+        {misdemeanours.map((misdemeanour: Misdemeanour) => (<>
+          <MisdemeanourRow citizenId={misdemeanour.citizenId} date={misdemeanour.date} misdemeanour={misdemeanour.misdemeanour}></MisdemeanourRow>
+        </>))}
       </table> 
     </div>
   </>)
