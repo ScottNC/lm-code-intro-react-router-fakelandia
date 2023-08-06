ee66b18577884e4ed3520e67872a39a8cb3244de
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-06 22:38:28 +0100

    show full midemeanour and emoji

--- client/src/components/MisdemeanourRow.tsx
+++ client/src/components/MisdemeanourRow.tsx
@@ -1,10 +1,18 @@
 import { Misdemeanour } from "../types/misdemeanours.types"
+import { MISDEMEANOURS } from "../types/misdemeanours.types"
+
+const misdemeanourDisplay : {[key in typeof MISDEMEANOURS[number]] : string} = {
+  rudeness: 'Mild Public Rudeness 🤪',
+  lift: 'Speaking in a Lift 🗣',
+  vegetables: 'Not Eating Your Vegetables 🥗',
+  united: 'Supporting Manchester United 😈'
+}
 
 export const MisdemeanourRow : React.FC<Misdemeanour> = ({citizenId, date, misdemeanour}) => (<>
   <tr className="table--row">
     <td className="table--row--item table--row--item--text">{citizenId}</td>
     <td className="table--row--item table--row--item--text">{date}</td>
-    <td className="table--row--item table--row--item--text">{misdemeanour}</td>
+    <td className="table--row--item table--row--item--text">{misdemeanourDisplay[misdemeanour]}</td>
     <td className="table--row--item table--row--item--text">
       <img src={`https://picsum.photos/200/150?random=${citizenId}`}/>
     </td>
