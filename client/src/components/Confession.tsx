75c69a2496e865bf58c03cf89e3cbe29b34a1e7d
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-06 23:21:00 +0100

    adds text to confession page

--- client/src/components/Confession.tsx
+++ client/src/components/Confession.tsx
@@ -1,1 +1,14 @@
-export const Confession : React.FC = () => <div className="text">Confession!</div>;
+export const Confession : React.FC = () => {
+
+  
+  return (
+    <div className="text">
+      <p className="text__paragraph">
+        It's very difficult to catch people committing midemeanours so we appreciate it when citizens confess to us directly.
+      </p>
+      <p className="text__paragraph">
+        However, if you're just having a hard day and need to vent the you're welcome to contact us here too. Up to you!
+      </p>
+    </div>
+  );
+}
