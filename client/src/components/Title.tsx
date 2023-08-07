336d4a364659382cc32459f73d3f968959a6ec76
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-07 01:19:36 +0100

    header is orange

--- client/src/components/Title.tsx
+++ client/src/components/Title.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export const Title: React.FC = () => {
   return (
     <div className='header__card'>
-      <h1 className='header__card--title header__card--link'><Link to="/">Fakelandia Justice Department</Link></h1>
+      <h1 className='header__card--title'><Link to="/" className='header__card--title--link'>Fakelandia Justice Department</Link></h1>
     </div>
   );
 };
