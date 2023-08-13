79898fa535037819e200bc03bfa051da80e788b2
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-13 13:24:37 +0100

    removes uncessary tsx file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { Navbar } from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home';
-import { Misdemeanours } from './components/Misdemeanours';
 import { Confession } from './components/Confession';
 import { NotFound } from './components/NotFound';
 import { Misdemeanour } from './types/misdemeanours.types';
 import { createContext, useMemo, useState } from 'react';
 import { Title } from './components/Title';
+import { MisdemeanourTable } from './components/MisdemeanourTable';
 
 export const MisdemeanoursContext = createContext<{ extraMisdemeanours : Misdemeanour[], setExtraMisdemeanours: React.Dispatch<React.SetStateAction<Misdemeanour[]>> }>({
   extraMisdemeanours: [],
@@ -27,7 +27,7 @@ function App() {
         <Routes>
           <Route path="/" Component={Home} />
           <Route path="/home" Component={Home} />
-          <Route path="/misdemeanour" Component={Misdemeanours} />
+          <Route path="/misdemeanour" Component={MisdemeanourTable} />
           <Route path="/confession" Component={Confession} />
           <Route path="/*" Component={NotFound} />
         </Routes>
--- client/src/components/Misdemeanours.tsx
+++
@@ -1,8 +0,0 @@
-import { MisdemeanourTable } from "./MisdemeanourTable";
-
-export const Misdemeanours : React.FC = () => {
-
-  return (<>
-    <MisdemeanourTable></MisdemeanourTable>
-  </>)
-};
