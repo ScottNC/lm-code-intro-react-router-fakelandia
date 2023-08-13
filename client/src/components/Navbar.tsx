265edefb0bd67f360e0f6e4e2d6962df977d6e0a
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-13 20:11:23 +0100

    nav bar is more accessible

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export const Navbar: React.FC = () => {
   return (
       <nav className='navbar__container'>
-        <li className='navbar__container--link'><Link to="/home">Home</Link></li>
-        <li className='navbar__container--link'><Link to="/misdemeanour">Misdemeanours</Link></li>
-        <li className='navbar__container--link'><Link to="/confession">Confess To Us</Link></li>
+        <menu className='menu'><li className='navbar__container--link'><Link to="/home">Home</Link></li></menu>
+        <menu className='menu'><li className='navbar__container--link'><Link to="/misdemeanour">Misdemeanours</Link></li></menu>
+        <menu className='menu'><li className='navbar__container--link'><Link to="/confession">Confess To Us</Link></li></menu>
       </nav>
   );
 };
