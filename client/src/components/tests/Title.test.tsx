9737a53a06aa2bb9bd522fcd8d0a44f0fc6f017d
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-13 13:08:09 +0100

    adds test for title and link

---
+++ client/src/components/tests/Title.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { Title } from '../Title';
+
+test('renders form title', () => {	
+  render(
+		<MemoryRouter>
+			<Title />
+		</MemoryRouter>
+	);
+
+  const fakelandiaTitle = screen.getByText(/Fakelandia Justice Department/i);
+  expect(fakelandiaTitle).toHaveAttribute('href', '/');
+});
