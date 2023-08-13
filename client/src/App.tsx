e10887e89c80c21ab8dc6052505efd4f20918db0
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-13 16:59:29 +0100

    removes unecessary component for form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { Navbar } from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home';
-import { Confession } from './components/Confession';
 import { NotFound } from './components/NotFound';
 import { Misdemeanour } from './types/misdemeanours.types';
 import { createContext, useMemo, useState } from 'react';
 import { Title } from './components/Title';
 import { MisdemeanourTable } from './components/MisdemeanourTable';
+import { ConfessionForm } from './components/ConfessionForm';
 
 export const MisdemeanoursContext = createContext<{ extraMisdemeanours : Misdemeanour[], setExtraMisdemeanours: React.Dispatch<React.SetStateAction<Misdemeanour[]>> }>({
   extraMisdemeanours: [],
@@ -28,7 +28,7 @@ function App() {
           <Route path="/" Component={Home} />
           <Route path="/home" Component={Home} />
           <Route path="/misdemeanour" Component={MisdemeanourTable} />
-          <Route path="/confession" Component={Confession} />
+          <Route path="/confession" Component={ConfessionForm} />
           <Route path="/*" Component={NotFound} />
         </Routes>
       </MisdemeanoursContext.Provider>
